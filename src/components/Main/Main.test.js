f2a1a5e2f272730e9c461b48b427c8f23ea42cee
Carly Sierra <carlylwomack@gmail.com>
2018-03-01 15:15:18 -0600

    imported shallow to Main, removed import mount from tests for now

--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './Main';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+import Main from './Main'
 
 describe('<Main />', () => {
   it('renders without crashing', () => {
