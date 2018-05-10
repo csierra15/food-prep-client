cfa7100fb008ccf02be75280e39f0da077614d40
Carly Sierra <carlylwomack@gmail.com>
2018-05-09 22:00:02 -0500

    styles, functionality changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
 const Footer = () => {
   return (
     <footer role="contentinfo">
       <span>Made by Carly Sierra</span>
+      <span><Link to="https://github.com/csierra15/food-prep-client"> <i className="fab fa-github xs"></i></Link></span>
     </footer>
   )
 }
